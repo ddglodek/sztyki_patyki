59e5232454d70178a303c187b8e069a759674cfb
Dawid Głodek <davidmk030@gmail.com>
2024-03-13 11:06:54 +0100

    shapes

--- kwadrat.py
+++ kwadrat.py
@@ -1,5 +1,22 @@
 bok_a = 10.5
-
 pole_kw = bok_a * bok_a
 print("Pole kwadratu wynosi: ", pole_kw)
-print("Pole kwadratu wynosi: ", pole_kw)
+
+
+bok_x = 10
+bok_y = 5
+pole_prostokata = bok_x * bok_y
+print("Pole prostokąta wynosi: ", pole_prostokata)
+
+
+bok_b = 20
+bok_h = 15
+pole_trojkata = (bok_b * bok_h)/2
+print("Pole trójkąta wynosi: ", pole_trojkata)
+
+
+promien_r = 10
+pi = 3.14
+pole_okregu = (pi*promien_r)**2
+pole = round(pole_okregu, 2)
+print("Pole okręgu wynosi: ", pole)
