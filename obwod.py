9b252c483ffbab244127de9917c027f70cef0ec0
Dawid Głodek <davidmk030@gmail.com>
2024-03-20 10:47:00 +0100

    ZADANIE_4

---
+++ obwod.py
@@ -0,0 +1,15 @@
+def obwod_kwadratu(a):
+    kwadrat_obliczenia = 4 * a
+    return kwadrat_obliczenia
+
+def obwod_okregu(r):
+    okrag_obliczenia = 2*3,14*r
+    return okrag_obliczenia
+
+def obwod_trapezu(a,b,c):
+    trapez_obliczenia = a + b + 2*c
+    return trapez_obliczenia
+
+def obwod_prostokata(a,b):
+    prostokat_obliczenia = 2*a + 2*b
+    return prostokat_obliczenia
